3092bb26c270558826da68fa802179f367bc0920
Anmol100 <mydatapack3@gmail.com>
2018-02-13 17:07:53 +0530

    first page header

---
+++ 001_first-page/jscript.js
@@ -0,0 +1,64 @@
+$(function(){
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+});
+
+// var windw = this;
+//
+// $.fn.followTo = function ( pos ) {
+//     var $this = this,
+//         $window = $(windw);
+//
+//     $window.scroll(function(e){
+//         if ($window.scrollTop() > pos) {
+//             $this.css({
+//                 position: 'absolute',
+//                 top: pos
+//             });
+//         } else {
+//             $this.css({
+//                 position: 'fixed',
+//                 top: 0
+//             });
+//         }
+//     });
+// };
+//
+// $('scroll-to').followTo(250);
